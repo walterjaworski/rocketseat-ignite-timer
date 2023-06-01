77d0a5b99e6f16bb0479b00d7b04633146cfb249
Walter Jaworski <walter.jaworski@gmail.com>
2023-06-01 14:37:12 -0300

    initial commit with eslint config

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ignite Timer
+
+Este é um projeto realizado no curso Ignite da [Rocketseat](https://www.rocketseat.com.br/).
+
+Trata-se de uma aplicação para aplicar a técnica pomodoro. Por se tratar de uma aplicação um pouco mais avançada do que o `Ignite Feed`, esta aplicação utiliza `styled component`, `eslint`, `react router dom`, `react hook form`, `react hooks` e `context api`.
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+import { Button } from './components/Button'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
